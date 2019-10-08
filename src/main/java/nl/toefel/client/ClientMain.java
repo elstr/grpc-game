aaa8ddf628a01cd2c990e0a400bbcbba70b1342f
Christophe Hesters <toefel18@gmail.com>
2019-10-08 23:33:32 +0200

    enabled execute task and removed outdated comment

--- src/main/java/nl/toefel/client/ClientMain.java
+++ src/main/java/nl/toefel/client/ClientMain.java
@@ -3,7 +3,6 @@ package nl.toefel.client;
 public class ClientMain {
     public static void main(String[] args) {
         // Running like this avoids SDK runtime errors
-        // See GameWindow.init() for initializations
         // https://stackoverflow.com/questions/52578072/gradle-openjfx11-error-javafx-runtime-components-are-missing
         GameWindow.main(args);
     }
